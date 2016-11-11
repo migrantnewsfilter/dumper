11297e415b48c8c60b4c0bd9d51e2f30ebfaad38
Nandan Rao <nandanmarkrao@gmail.com>
2016-11-11 18:47:30 +0100

    updating dump location

--- dump.sh
+++ dump.sh
@@ -5,4 +5,4 @@ BUCKET=migrantnews-app-db-dev-dumps
 
 mongoexport -h $MONGO_HOST --db newsfilter --collection news | aws s3 cp --no-sign-request - s3://$BUCKET/news/$FILE --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
-mongoexport -h $MONGO_HOST --db newsfilter --collection terms | aws s3 cp --no-sign-request - s3://$BUCKET/terms/$FILE --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+#mongoexport -h $MONGO_HOST --db newsfilter --collection terms | aws s3 cp --no-sign-request - s3://$BUCKET/terms/$FILE --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
