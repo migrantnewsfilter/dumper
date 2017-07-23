d4a64c878c8099ceab345fa8eb9e0996571ea6a2
Nandan Rao <nandanmarkrao@gmail.com>
2017-07-23 22:16:09 +0200

    Fixed CLI S3 argument

--- dump.sh
+++ dump.sh
@@ -3,6 +3,6 @@
 FILE=$(date +%Y-%m-%dT%H:%M:%SZ).json
 BUCKET=migrantnews-app-db-dev-dumps
 
-mongoexport -h $MONGO_HOST --db newsfilter --collection news | aws s3 cp s3://$BUCKET/news/$FILE --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+mongoexport -h $MONGO_HOST --db newsfilter --collection news | aws s3 cp - s3://$BUCKET/news/$FILE --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
-mongoexport -h $MONGO_HOST --db newsfilter --collection terms | aws s3 cp s3://$BUCKET/terms/$FILE --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+mongoexport -h $MONGO_HOST --db newsfilter --collection terms | aws s3 cp - s3://$BUCKET/terms/$FILE --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
