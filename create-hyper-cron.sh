1839bb2204e143c900dcace4416c8638eed1d95e
Nandan Rao <nandanmarkrao@gmail.com>
2017-08-29 08:59:14 +0200

    fixed env var for mongo host

--- create-hyper-cron.sh
+++ create-hyper-cron.sh
@@ -1,1 +1,1 @@
-hyper cron create --minute=0 --hour=23 --name dumper --env MONGO_HOST=mongodb://mongo:27017 --env-file .env --link news-mongo-1:mongo --size s2 migrantnewsfilter/dumper
+hyper cron create --minute=0 --hour=23 --name dumper --env-file .env --link news-mongo-1:mongo --size s2 migrantnewsfilter/dumper
